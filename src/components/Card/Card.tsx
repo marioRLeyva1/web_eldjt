2b73c5ae2e90f855d7a41fa60790d55afd9e0a46
marioRLeyva1 <mario_980615@hotmail.com>
2022-06-18 15:59:32 -0500

    layout added to other pages

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,7 +10,7 @@ const Card = (props: any) => {
           <p className='text-slate-600 text-center'>{props.subtitle}</p>
         </div>
         <button className='flex items-center justify-center text-xs border-2 rounded px-3 py-1 mt-1 mb-3 bg-secondary text-white shadow sm:text-base sm:rounded-lg '>
-            <p>LEER MÁS</p>
+            <p>{props.buttonLabel ? `${props.buttonLabel}` : 'LEER MÁS'}</p>
             <img src={Arrow} alt="arrow" className='-rotate-90 w-4'/>
         </button>
     </div>
--- src/screens/Admissions/AdmissionProcess.tsx
+++ src/screens/Admissions/AdmissionProcess.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Layout from '../../components/Layout/Layout'
+import Input from '../../components/Input/Input'
 
 const AdmissionProcess = () => {
   return (
-    <div>AdmissionProcess</div>
+    <Layout location='admissions'>
+      <div className='flex flex-col mt-24 items-center justify-center w-1024 mb-14'>
+          <div className=''>
+              <h1 className='text-3xl sm:text-5xl'>Proceso de admisión</h1>
+          </div>
+          {/* <div className='hidden sm:block w-16'>
+            <img src="https://firebasestorage.googleapis.com/v0/b/eldjt-web.appspot.com/o/misc%2Feldjt-png.png?alt=media&token=a31fec81-e6e8-42af-a263-735b9c7cb4cb" alt="eldjt" />
+          </div> */}
+          <div className='sm:mt-10 mt-7'>
+              
+          </div>
+          <div className='sm:flex justify-center items-center sm:w-1024 sm:mt-10'>
+            <img className='w-96' src="https://firebasestorage.googleapis.com/v0/b/eldjt-web.appspot.com/o/misc%2Feldjt-png.png?alt=media&token=a31fec81-e6e8-42af-a263-735b9c7cb4cb" alt="eldjt" />
+            <div className='sm:flex flex-col gap-4 mt-5 sm:w-96 hidden'>
+                <h3 className='text-2xl text-center'>Contáctanos</h3>
+                <Input type='text' placeholder={'Nombre Completo'} required={true}/>
+                <Input type='email' placeholder={'Correo Electrónico'} required={true}/>
+                <Input type='tel' placeholder={'Teléfono'} required={true}/>
+                <Input type='text' placeholder={'Ciudad'} required={true}/>
+                <button className='mx-5 mb-5 border-LG p-2 bg-secondary rounded text-white font-bold drop-shadow'>ENVIAR</button>
+            </div>
+          </div>
+      </div>
+    </Layout>
   )
 }
 
