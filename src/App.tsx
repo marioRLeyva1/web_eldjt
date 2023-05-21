d234722c2b6f0462394db685eb947f908b3f74a6
marioRLeyva1 <mario_980615@hotmail.com>
2023-05-20 20:45:08 -0600

    react helmet added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import MobileHeader from './components/MobileHeader/MobileHeader';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <div className='bg-light'>
-
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>Escuela Libre de Derecho y Jurisprudencia de Tabasco</title>
+                {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+                <meta name="description" content="Ser el mejor abodad@, inicia con la decisión correcta." />
+      </Helmet>
     </div>
   );
 }
